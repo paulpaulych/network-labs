f815eeacd1b9d28cdfaef32cdcbc0db81112876e
paulpaulych <Pasha21215@gmail.com>
2019-09-19 21:57:17 +0700

    + ip arg

--- lab1-clone-detector/src/main/java/Main.java
+++ lab1-clone-detector/src/main/java/Main.java
@@ -5,6 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        new CloneDetector().run();
+        new CloneDetector(args[0]).run();
     }
 }
